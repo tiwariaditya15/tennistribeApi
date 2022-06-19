8dc03bae5a16a580f8b074d60e813ea343b92c5e
Aditya Tiwari <tiwariaditya15@hotmail.com>
2022-06-20 01:35:31 +0530

    refactor: added explore route

--- routes/posts.router.ts
+++ routes/posts.router.ts
@@ -4,11 +4,13 @@ import {
   getFeed,
   getPost,
   deletePost,
+  getExploreFeed,
 } from "../controllers/posts.controller";
 
 const router = express.Router();
 
 router.route("/").get(getFeed);
+router.route("/explore").get(getExploreFeed);
 router.route("/:postId").get(getPost);
 router.route("/").post(newPost);
 router.route("/delete").post(deletePost);
